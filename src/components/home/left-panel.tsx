6c6117cc2dc902e964c524724ce8bf29f0e44fb0
prinzeval <valentineallpowers@gmail.com>
2025-03-07 12:19:15 +0200

    ok its time

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -1,9 +1,8 @@
 import { ListFilter, LogOut, MessageSquareDiff, Search, User } from "lucide-react";
 import { Input } from "../ui/input";
-import ThemeSwitch from "@/components/home/theme-switch";
-
+import ThemeSwitch from "./theme-switch";
+import Conversation from "./conversation";
 const LeftPanel = () => {
-	const conversations = [];
 
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
@@ -38,6 +37,9 @@ const LeftPanel = () => {
 			{/* Chat List */}
 			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
 				{/* Conversations will go here*/}
+                {conversations.map((conversation) => (
+                    <Conversation key={conversation._id} conversation={conversation} />
+                ))}
 
 				{conversations?.length === 0 && (
 					<>
