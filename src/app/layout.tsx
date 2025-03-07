82c8dd991530df0c1011b5e36f2c35fdafcf90d5
prinzeval <valentineallpowers@gmail.com>
2025-03-07 22:40:03 +0200

    ongoing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
-import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,9 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-        <ConvexClientProvider>
           {children}
-        </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
